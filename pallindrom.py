e0c40b580e7383c2197cdc633e23098033a95a4f
Ashutoshrai14 <49158455+Ashutoshrai14@users.noreply.github.com>
2020-02-03 14:31:57 +0530

    Add files via upload

---
+++ pallindrom.py
@@ -0,0 +1,12 @@
+st=input("Enter a string:")
+k=len(st)
+temp=0
+while(
+	while(k!=0):
+		if(st[i]==st[k]):
+			temp=temp+1
+		k=k-1
+if(temp==len(st)):
+	print("It is pallindrome!")
+else:
+	print("It is not pallindrome!")
